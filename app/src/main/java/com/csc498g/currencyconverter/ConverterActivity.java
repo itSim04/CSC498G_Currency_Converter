7cebaca45f27e3f338894b893843cf46f4acb30b
Simon Mouawad <moawadsimon@gmail.com>
2022-09-11 16:54:19 +0300

    Created a basic function that converts a currency to Dollars. Changed floats to doubles

--- app/src/main/java/com/csc498g/currencyconverter/ConverterActivity.java
+++ app/src/main/java/com/csc498g/currencyconverter/ConverterActivity.java
@@ -3,15 +3,16 @@ package com.csc498g.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class ConverterActivity extends AppCompatActivity {
 
-    static final HashMap<String, Float> currencies = new HashMap<>() {{
+    static final HashMap<String, Double> currencies = new HashMap<>() {{
 
-        put("USD", 1.0F);
-        put("LBP", 40000.0F);
+        put("USD", 1.0);
+        put("LBP", 40000.0);
 
     }};
 
@@ -21,26 +22,28 @@ public class ConverterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_converter);
     }
 
-    public int convert(float amount, String currency1, String currency2) {
+    public double convert(float amount, String currency1, String currency2) {
 
         // Convert from Currency 1 to Currency 2
 
-        int cur1_to_dollars = toDollar(amount, currency1);
-        int dollars_to_cur2 = fromDollar(cur1_to_dollars, currency2);
+        double cur1_to_dollars = toDollar(amount, currency1);
+        double dollars_to_cur2 = fromDollar(cur1_to_dollars, currency2);
         return dollars_to_cur2;
 
     }
 
-    private int fromDollar(float amount, String currency2) {
-
-        // Convert from Dollars to a certain currency
-        return 0;
-    }
-
-    private int toDollar(float amount, String currency1) {
+    private double toDollar(double amount, String currency1) {
 
         // Convert from a certain currency to Dollars
-        return 0;
-        
+        return amount / currencies.get(currency1);
+
     }
+
+    private double fromDollar(double amount, String currency2) {
+
+        // Convert from Dollars to a certain currency
+
+        return 0;
+    }
+
 }
